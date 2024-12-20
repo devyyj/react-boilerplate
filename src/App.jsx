2c66afad71f9731987d082eefe663ea01755915b
devyyj <devyyj@gmail.com>
2024-12-20 16:51:16 +0900

    초기설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import {Button, Container} from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(0)
+    // 카카오 로그인 API 호출
+    const handleKakaoLogin = async () => {
+        window.location.href = "http://localhost:8080/oauth2/authorization/kakao"
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Container>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleKakaoLogin}
+                size="large"
+            >
+                버튼
+            </Button>
+        </Container>
+    );
 }
 
-export default App
+export default App;
